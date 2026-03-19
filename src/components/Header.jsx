20076cac9490ab00e0247c70c8732a9b4186b6e3
sirthanwat <36045599+sirthanwat@users.noreply.github.com>
2026-03-19 22:12:09 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
 
+const Header = () => (
+  <header className="header">
+    <div className="container nav">
+      <div className="logo">QAElevate</div>
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/services">Services</Link>
+        <Link to="/faq">FAQ</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+    </div>
+  </header>
+);
+
+export default Header;

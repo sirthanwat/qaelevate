fd9564394825631dc0aa8d06686b39714ea5f324
sirthanwat <36045599+sirthanwat@users.noreply.github.com>
2026-03-19 22:17:48 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,1 +1,10 @@
+import React from "react";
 
+const Contact = () => (
+  <section className="section container">
+    <h1>Contact Us</h1>
+    <p>Email: support@qaelevate.com</p>
+  </section>
+);
+
+export default Contact;

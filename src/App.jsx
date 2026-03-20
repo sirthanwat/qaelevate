4b40de9527a97020e96db06b1f287b84312cf5af
sirthanwat <36045599+sirthanwat@users.noreply.github.com>
2026-03-20 17:01:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Experience from "./components/Experience";
+import Features from "./components/Features";
 import CTA from "./components/CTA";
 
 import Home from "./pages/Home";
